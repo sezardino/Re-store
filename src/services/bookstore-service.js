a7b6086e452db732b1d266520c898d99cb6e1d19
Edward <e.arekhva@gmail.com>
2020-09-04 19:50:00 +0200

    add book service

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,8 @@
+export default class BookstoreService {
+	getBooks() {
+		return [
+			{id: 1, title: 'Test Title 1', author: 'Test Author 1'},
+			{id: 2, title: 'Test Title 2', author: 'Test Author 2'},
+		];
+	}
+}
